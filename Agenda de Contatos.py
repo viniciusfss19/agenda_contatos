8bdc3e47904f66ad8fceb725288f435e3c9cdec1
viniciusfss19 <91830940+viniciusfss19@users.noreply.github.com>
2022-05-28 15:34:34 -0300

    busca_contato

--- Agenda de Contatos.py
+++ Agenda de Contatos.py
@@ -7,7 +7,8 @@ def menu():
     [1] CADASTRAR CONTATOS
     [2] LISTAR CONTATOS
     [3] DELETAR CONTATO
-    [4] BUSCAR CONTATO PELO ID
+    [4] BUSCAR CONTATO PELO INOME
+    [5] SAIR
     ===============================================================    
     ESCOLHA UMA DAS OPÇÕES A CIMA:
     ''')
@@ -19,20 +20,22 @@ def menu():
         listarcontato()
     elif opcao == "3":
         deletarcontato()
+    elif opcao == "4":
+        buscarcontato()
     else:
-        buscarcontato() 
+        sair()
 
 
 def cadastrarcontato():
-     nome= str(input('Nome do contato'))
-     idcontato= int(input('Adicione um codigo ID para o contato'))
-     telefone=input('Número de Telefone')
-     endereco=input('Endereço')
+     nome_contato = str(input('Nome do contato'))
+     idcontato = int(input('Adicione um codigo ID para o contato'))
+     telefone =input('Número de Telefone')
+     endereco =input('Endereço')
      numeroresidencia=input("Digite o número da residência")
      email=input('E-mail')
      try:
         agenda = open("agenda.txt","a") #'a' é para disponibilizar a agenda para edição#
-        dados = f'{nome};{idcontato};{telefone};{endereco};{numeroresidencia};{email}\n'
+        dados = f'{nome_contato};{idcontato};{telefone};{endereco};{numeroresidencia};{email}\n'
         agenda.write(dados)
         agenda.close()
         print('Contato Salvo com Sucesso :)')
@@ -43,13 +46,29 @@ def listarcontato():
     agenda = open('agenda.txt','r') #'r' é para disponibilizar a agenda para leitura#
     for contato in agenda:
         print(contato)
-
+    agenda.close()
 
 def deletarcontato():
     print(f'DELETAR O CONTATO')
 
 def buscarcontato():
-    print(f'BUSCAR CONTATO')
+    nome = input('Digite o nome do contato: ')
+    nome = nome.upper().strip()
+    agenda = open('agenda.txt', 'r')  # 'r' é para disponibilizar a agenda para leitura#
+
+    for contato in agenda:
+        if nome in contato.split(":")[0]:
+          print(contato)
+        else:
+            print('contato inexistente')
+
+
+    agenda.close()
+
+def sair():
+    print(f'ATÉ MAIS')
+    exit()
+
 
 def main():
     menu()
